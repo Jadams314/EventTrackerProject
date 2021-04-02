e27c4756fa55a4f5931d4ae505272f8ca8bdfbbb
Justin <>
2021-04-02 16:05:43 -0500

    Stubbed boot project

---
+++ RideTracker/src/test/java/com/skilldistillery/rideTracker/RideTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.rideTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
